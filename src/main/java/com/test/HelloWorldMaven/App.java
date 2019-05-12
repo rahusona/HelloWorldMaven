89d3a1e9e7ec0a0ee4fe69c62096ebbe69625bc7
rahusona <46642668+rahusona@users.noreply.github.com>
2019-05-12 10:17:27 +0530

    Update App.java

--- src/main/java/com/test/HelloWorldMaven/App.java
+++ src/main/java/com/test/HelloWorldMaven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Rahul it sunday!!" );
+        System.out.println( "Hello Rahul it's sunday!!" );
     }
 }
